61910bc0f0a1da5165931c7d14f8fe9f03370ca8
Mscourtneybryant <87495361+Mscourtneybryant@users.noreply.github.com>
2024-07-16 15:14:05 -0500

    303.9.2 complete

---
+++ src/main/java/org/example/Doctor.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Doctor {
+    String DoctorName;
+    String Department;
+    public void Doctor_Details() {
+        System.out.println("Doctor Details...");
+    }
+}
---
+++ src/main/java/org/example/Surgeon.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Surgeon extends Doctor {
+    void Surgeon_Details() {
+        System.out.println("Surgeon  Detail...");
+        System.out.println(Department = "Cardio");
+    }
+}
